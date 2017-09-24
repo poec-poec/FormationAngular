e60fe487645bbf05e37f1eafab00dae06b155ef1
ChristopheGueroult <30533056+ChristopheGueroult@users.noreply.github.com>
2017-09-24 18:25:18 +0200

    Update environment.prod.ts

--- my-app/src/environments/environment.prod.ts
+++ my-app/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyA2IJTD92Woc8kxgknyNUJLhe71bxly6M0",
-    authDomain: "angularbordeau.firebaseapp.com",
-    databaseURL: "https://angularbordeau.firebaseio.com",
-    projectId: "angularbordeau",
-    storageBucket: "angularbordeau.appspot.com",
-    messagingSenderId: "475070932037"
+    apiKey: "your api key",
+    authDomain: "yout domaine.firebaseapp.com",
+    databaseURL: "your database url",
+    projectId: "your project",
+    storageBucket: "your storageBucket",
+    messagingSenderId: "yout messagingSenderId"
   }
 };
